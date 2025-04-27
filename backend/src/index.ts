516206c0b78ab6633de15084c9b4d42f1a5f05cc
RBaibhav <r.baibhav2023@gmail.com>
2025-04-27 15:08:45 +0530

    routes and minor changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,24 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.post("api/v1/signup", (req, res) => {});
+
+app.post("/api/v1/login", (req, res) => {});
+
+app.post("/api/v1/content", (req, res) => {});
+
+app.get("/api/v1/content", (req, res) => {});
+
+app.delete("/api/v1/content", (req, res) => {});
+
+app.post("/api/v1/brain/share", (req, res) => {});
+
+app.get("/api/v1/brain/:sharelink", (req, res) => {});
